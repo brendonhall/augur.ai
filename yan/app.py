8b3a5886136f31f9128b365fed7b8888093724c2
Yan <yan@fastmail.com>
2017-09-23 17:47:14 -0500

    Multiple axes

--- yan/app.py
+++ yan/app.py
@@ -11,6 +11,11 @@ from io_helpers import sensors_explanation
 
 esp_data_filename = 'esp.pkl'
 data = pd.read_pickle(esp_data_filename)
+data.fillna(method='pad', inplace=True)
+data.fillna(method='bfill', inplace=True)
+#for c in data.columns:
+#    data[c].fillna(data[c].mode()[0], inplace=True)
+
 
 levels = data.columns.levels
 fields_to_choose = levels[0].values
@@ -37,10 +42,10 @@ app.layout = html.Div([
     ], style={'width': '49%', 'display': 'inline-block'}),
 
     html.Label('Sensor'),
-    dcc.RadioItems(
+    dcc.Checklist(
         id='sensor-selected',
         options=[{'label': sensors_explanation[s], 'value': s} for s in sensors_to_choose],
-        value=str(sensors_to_choose[0]),
+        values=[str(sensors_to_choose[0])],
         style={'columnCount': 5}
     ),
 
@@ -52,35 +57,76 @@ app.layout = html.Div([
     dash.dependencies.Output('sensor-readings', 'figure'),
     [dash.dependencies.Input('field-selected', 'value'),
      dash.dependencies.Input('pump-selected', 'value'),
-     dash.dependencies.Input('sensor-selected', 'value')])
-def update_graph(field, pump, sensor):
-    sensor_data = data[int(field), int(pump), sensor]
+     dash.dependencies.Input('sensor-selected', 'values')])
+def update_graph(field, pump, sensors):
+    sensor_data = data[int(field), int(pump)]
     times = sensor_data.index.values
     time_to_plot = [datetime.fromtimestamp(t) for t in times] #[(t - times[0])/60 for t in times]
-    readings = sensor_data.values
 
-    return {
-        'data': [go.Scatter(
+    if len(sensors) == 1:
+        my_data = [go.Scatter(
             x=time_to_plot,
-            y=readings,
-        )],
-        'layout': go.Layout(
+            y=sensor_data[sensors[0]].values,
+            name=sensors_explanation[sensors[0]],
+        )]
+
+        my_layout = go.Layout(
             xaxis={
-                'title': 'Time since start [mins]',
-                'type': 'time'
+                'title': 'Date',
+                'type': 'time',
+                'domain': [0.15, 1]
             },
             yaxis={
-                'title': sensors_explanation[sensor],
-                'type': 'linear' #if yaxis_type == 'Linear' else 'log'
+                'title': sensors_explanation[sensors[0]],
+                'type': 'linear'
             },
             margin={'l': 80, 'b': 40, 't': 10, 'r': 0},
             hovermode='closest'
         )
+    else:
+        my_data = [
+            go.Scatter(
+                x=time_to_plot,
+                y=sensor_data[sensors[0]].values,
+                name=sensors_explanation[sensors[0]],
+            ),
+            go.Scatter(
+                x=time_to_plot,
+                y=sensor_data[sensors[1]].values,
+                name=sensors_explanation[sensors[1]],
+                yaxis='y2'
+            )]
+
+        my_layout = go.Layout(
+            xaxis={
+                'title': 'Date',
+                'type': 'time',
+                'domain': [0.15, 1]
+            },
+            yaxis={
+                'title': sensors_explanation[sensors[0]],
+                'type': 'linear'
+            },
+            yaxis2={
+                'title': sensors_explanation[sensors[1]],
+                'type': 'linear',
+                'overlaying': 'y',
+                'side': 'left',
+                'position': 0,
+                'anchor': 'free'
+            },
+            margin={'l': 80, 'b': 40, 't': 10, 'r': 0},
+            hovermode='closest'
+        )
+
+    return {
+        'data': my_data,
+        'layout': my_layout
     }
 
 
 app.css.append_css({
-    "external_url": 'https://cdnjs.cloudflare.com/ajax/libs/skeleton-framework/1.1.1/skeleton.min.css'
+   "external_url": 'https://cdnjs.cloudflare.com/ajax/libs/skeleton-framework/1.1.1/skeleton.min.css'
 })
 
 if __name__ == '__main__':
