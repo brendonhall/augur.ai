1ea8553d1212204061a0188bdaaa312015c9ddf3
Yan <yan@fastmail.com>
2017-09-24 14:52:40 -0500

    Reactive plotting

--- yan/app.py
+++ yan/app.py
@@ -1,125 +1,121 @@
 from datetime import datetime
 import pandas as pd
-
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
 import plotly.graph_objs as go
+from plotly import tools
 
-from io_helpers import sensors_explanation
+from io_helpers import sensors_explanation, H5_File
 
 
-esp_data_filename = 'esp.pkl'
-data = pd.read_pickle(esp_data_filename)
-data.fillna(method='pad', inplace=True)
-data.fillna(method='bfill', inplace=True)
+h5_file = H5_File('esp_data_interp.h5')
+data_description = h5_file.data_description()
 
-levels = data.columns.levels
-fields_to_choose = levels[0].values
-pumps_to_choose = levels[1].values
-sensors_to_choose = levels[2].values
+fields_to_choose = sorted(data_description.keys(), key=lambda x: int(x))
 
 app = dash.Dash()
+
+current_selection = {'field': None,
+                     'pump': None}
 
 app.layout = html.Div([
     html.Div([
         html.Label('Field #'),
         dcc.Dropdown(
             id='field-selected',
-            options=[{'label': str(f), 'value': str(f)} for f in fields_to_choose],
-            value=str(fields_to_choose[0]))
+            options=[{'label': f, 'value': f} for f in fields_to_choose],
+            value=None
+        )
     ], style={'width': '49%', 'display': 'inline-block'}),
 
     html.Div([
         html.Label('Pump #'),
-        dcc.Dropdown(
-            id='pump-selected',
-            options=[{'label': str(p), 'value': str(p)} for p in pumps_to_choose],
-            value=str(pumps_to_choose[0]))
+        dcc.Dropdown(id='pump-selected')
     ], style={'width': '49%', 'display': 'inline-block'}),
 
-    html.Label('Sensor'),
+    html.Label('Sensors'),
     dcc.Checklist(
         id='sensor-selected',
-        options=[{'label': sensors_explanation[s], 'value': s} for s in sensors_to_choose],
-        values=[str(sensors_to_choose[0])],
+        options=[],
+        values=[],
         style={'columnCount': 5}
     ),
 
     dcc.Graph(id='sensor-readings')
 ])
 
+@app.callback(
+    dash.dependencies.Output('pump-selected', 'options'),
+    [dash.dependencies.Input('field-selected', 'value')]
+)
+def set_pump_options(field):
+    if field is None:
+        return []
+    current_selection['field'] = field
+    options = [{'label': i, 'value': i} for i in sorted(data_description[field].keys(),
+                                                        key=lambda x: int(x))]
+    print(options)
+    return options
+
+
+@app.callback(
+    dash.dependencies.Output('pump-selected', 'value'),
+    [dash.dependencies.Input('pump-selected', 'options')]
+)
+def set_pump_value(available_options):
+    if not available_options:
+        return None
+    return available_options[0]['value']
+
+
+@app.callback(
+    dash.dependencies.Output('sensor-selected', 'options'),
+    [dash.dependencies.Input('pump-selected', 'value')]
+)
+def set_sensor_options(pump):
+    field = current_selection['field']
+    if field is None or pump is None:
+        return []
+    current_selection['pump'] = pump
+    options = set(data_description[field][pump]) - set(['TCPU', 'PLIN', 'TVFD1', 'EINPA', 'JOUT', 'YSD', 'KRUNRSAC', 'EVFD1OUT'])
+    return [{'label': sensors_explanation[i], 'value': i} for i in options]
+
+
+@app.callback(
+    dash.dependencies.Output('sensor-selected', 'values'),
+    [dash.dependencies.Input('sensor-selected', 'options')]
+)
+def set_sensor_values(available_options):
+    return []
+
 
 @app.callback(
     dash.dependencies.Output('sensor-readings', 'figure'),
-    [dash.dependencies.Input('field-selected', 'value'),
-     dash.dependencies.Input('pump-selected', 'value'),
-     dash.dependencies.Input('sensor-selected', 'values')])
-def update_graph(field, pump, sensors):
-    sensor_data = data[int(field), int(pump)]
-    times = sensor_data.index.values
-    time_to_plot = [datetime.fromtimestamp(t) for t in times] #[(t - times[0])/60 for t in times]
+    [dash.dependencies.Input('sensor-selected', 'values')])
+def update_graphs(sensors):
+    field = current_selection['field']
+    pump = current_selection['pump']
+    if field is None or pump is None or not sensors:
+        return {}
 
-    if len(sensors) == 1:
-        my_data = [go.Scatter(
-            x=time_to_plot,
-            y=sensor_data[sensors[0]].values,
-            name=sensors_explanation[sensors[0]],
-        )]
+    sensor_data = h5_file.query(field, pump, sensors[0]).value
+    fig = tools.make_subplots(rows=len(sensors), cols=1,
+                              subplot_titles=tuple(s for s in sensors),
+                              shared_xaxes=True, shared_yaxes=False,
+                              vertical_spacing=0.2)
 
-        my_layout = go.Layout(
-            xaxis={
-                'title': 'Date',
-                'type': 'time',
-                'domain': [0.15, 1]
-            },
-            yaxis={
-                'title': sensors_explanation[sensors[0]],
-                'type': 'linear'
-            },
-            margin={'l': 80, 'b': 40, 't': 10, 'r': 0},
-            hovermode='closest'
+    for i, sensor in enumerate(sensors):
+        sensor_data = h5_file.query(field, pump, sensor).value
+        s_data = go.Scatter(
+            x=sensor_data[0,:],
+            y=sensor_data[1,:],
+            name=sensors_explanation[sensor],
         )
-    else:
-        my_data = [
-            go.Scatter(
-                x=time_to_plot,
-                y=sensor_data[sensors[0]].values,
-                name=sensors_explanation[sensors[0]],
-            ),
-            go.Scatter(
-                x=time_to_plot,
-                y=sensor_data[sensors[1]].values,
-                name=sensors_explanation[sensors[1]],
-                yaxis='y2'
-            )]
+        fig.append_trace(s_data, i+1, 1)
 
-        my_layout = go.Layout(
-            xaxis={
-                'title': 'Date',
-                'type': 'time',
-                'domain': [0.15, 1]
-            },
-            yaxis={
-                'title': sensors_explanation[sensors[0]],
-                'type': 'linear'
-            },
-            yaxis2={
-                'title': sensors_explanation[sensors[1]],
-                'type': 'linear',
-                'overlaying': 'y',
-                'side': 'left',
-                'position': 0,
-                'anchor': 'free'
-            },
-            margin={'l': 80, 'b': 40, 't': 10, 'r': 0},
-            hovermode='closest'
-        )
-
-    return {
-        'data': my_data,
-        'layout': my_layout
-    }
+    fig['layout'].update(height=800)
+    return fig
 
 
 app.css.append_css({
